b98df46d72835b92c05fb195c8ef1e0524d35ee1
Maciej Kaczmarek <maciej@kaczmarek.io>
2022-02-11 08:06:12 +0100

    Remove unused type

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,3 @@
-export type DeepPartial<Type> = Type extends object ? { [P in keyof Type]?: DeepPartial<Type[P]> } : Type
-
 export type RecordWithSuffix<BaseType, Type, Suffix extends string> = {
   [P in (keyof BaseType & string) as `${P}${Suffix}`]: Type
 }
